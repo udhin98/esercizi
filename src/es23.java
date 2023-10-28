2a61ca3dcee1a8989272b4920f89864c6a18924e
Pritvi <udhin1998@gmail.com>
2023-10-28 12:59:14 +0200

    es23

--- src/es23.java
+++ src/es23.java
@@ -1,16 +1,14 @@
-public class es22 {
+public class es23 {
     public static void main(String[] args) {
         int numero = 10;
         stop(numero);
     }
     public static void stop (int numero) {
         for (int i=0; i<=numero; i++) {
-
             if (i==5) {
-                break;
+                continue;
             }
             System.out.println(i);
         }
     }
 }
-
